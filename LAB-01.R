79e9d83663c64ff680649d7fb32ee253a2095f03
Coutoumanos <jhc2699@austin.eid.utexas.edu>
2017-09-06 10:54:37 -0500

    Lab 01

---
+++ LAB-01.R
@@ -0,0 +1,20 @@
+a1 <- 140
+a1 <- (a1+5)/5
+a2 <- a1^2+5
+a3 <- a1+a2
+paste(a1,a2,a3)
+
+classIQ <- c(100,160,70,80)
+classIQ[3]+5
+classIQ[3] <- classIQ[3]+5
+classIQ_colon <- c(50:75)
+classIQ_rep <- rep(85, 10)
+classIQ_seq <- seq(from=45, to=105, by=5)
+
+women
+women$weight
+
+meanHeight <- sum(women$height)/length(women$height)
+mm <- mean(women$height)
+paste(meanHeight,mm)
+
